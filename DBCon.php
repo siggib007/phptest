b0310a8552b5ab81439d1af70a4ed617753914a4
Siggi@Reba <siggi@supergeek.us>
2022-06-02 19:33:21 +0000

    more tweaks

--- DBCon.php
+++ DBCon.php
@@ -1,15 +1,14 @@
 <?php
-$UID = getenv("MYSQL_USER") ;
-$DBServerName = getenv("MYSQL_HOST") ;
-$PWD = getenv("MYSQL_PASSWORD") ;
-$DefaultDB = "VMdb" ;
+  require("secrets.php")
+  $DefaultDB = "VMdb" ;
+  $MailHostPort = 587;
 
-$strRemoteIP = $_SERVER["REMOTE_ADDR"];
-$dbh= new mysqli ($DBServerName, $UID, $PWD, $DefaultDB);
-if ($dbh->connect_errno)
-{
-    error_log( "Failed to connect to MySQL. Error(" . $dbh->connect_errno . ") " . $dbh->connect_error);
-    exit;
-}
+  $strRemoteIP = $_SERVER["REMOTE_ADDR"];
+  $dbh= new mysqli ($DBServerName, $UID, $PWD, $DefaultDB);
+  if ($dbh->connect_errno)
+  {
+      error_log( "Failed to connect to MySQL. Error(" . $dbh->connect_errno . ") " . $dbh->connect_error);
+      exit;
+  }
 ?>
 
---
+++ secrets.php
@@ -0,0 +1,8 @@
+<?php
+  $UID = getenv("MYSQL_USER");
+  $DBServerName = getenv("MYSQL_HOST");
+  $PWD = getenv("MYSQL_PASSWORD");
+  $MailUser = '';
+  $MailPWD = '';
+  $MailHost = '';
+?>
