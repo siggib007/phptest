70b7de87bcbe2ceeff4d46f85592b7e5447b90fa
Siggi@Reba <siggi@supergeek.us>
2022-06-11 23:18:01 +0000

    moved code into function

--- AKeylessTest.php
+++ AKeylessTest.php
@@ -1,43 +1,67 @@
 <?php
-  print "<h1>Fetching secret from AKEYLESS secret management system at akeyless.io</h1>";
-  $SecretName = "MyFirstSecret";
-  print "<p>Fetching secret named $SecretName</p>";
-  $AccessID = getenv("KEYLESSID");
-  $AccessKey = getenv("KEYLESSKEY");
-  $url = "https://rest.akeyless-security.com/auth?access-id=$AccessID&access-type=api_key&access-key=$AccessKey";
-  $curl = curl_init();
-  $curlOpt = array(
-    CURLOPT_URL => $url,
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => '',
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 0,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => 'POST',
-    CURLOPT_HTTPHEADER => array('accept: application/json'),);
-  curl_setopt_array($curl, $curlOpt);
-  $response = curl_exec($curl);
-  curl_close($curl);
-  $arrResponse = json_decode($response, TRUE);
-  $token = $arrResponse["token"];
-  $url = "https://rest.akeyless-security.com/get-secret-value?name=$SecretName&token=$token";
-  $curl = curl_init();
-  $curlOpt = array(
-    CURLOPT_URL => $url,
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => '',
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 0,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => 'POST',
-    CURLOPT_HTTPHEADER => array('accept: application/json'),);
-  curl_setopt_array($curl, $curlOpt);
-  $response = curl_exec($curl);
-  curl_close($curl);
+  print "<h1>Fetching secret from AKEYLESS secret management system at akeyless.io</h1>\n";
+  
+  function FetchKeylessStatic ($arrNames)
+  {
+    $AccessID = getenv("KEYLESSID");
+    $AccessKey = getenv("KEYLESSKEY");
+    $url = "https://rest.akeyless-security.com/auth?access-id=$AccessID&access-type=api_key&access-key=$AccessKey";
+    $curl = curl_init();
+    $curlOpt = array(
+      CURLOPT_URL => $url,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => '',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => 'POST',
+      CURLOPT_HTTPHEADER => array('accept: application/json'),);
+    curl_setopt_array($curl, $curlOpt);
+    $response = curl_exec($curl);
+    curl_close($curl);
+    $arrResponse = json_decode($response, TRUE);
+    $token = $arrResponse["token"];
+    
+    $arrValues = array();
+    foreach ($arrNames as $SecretName) 
+    {
+      print "<p>Fetching secret named $SecretName</p>\n";
+      $url = "https://rest.akeyless-security.com/get-secret-value?name=$SecretName&token=$token";
+      $curl = curl_init();
+      $curlOpt = array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_HTTPHEADER => array('accept: application/json'),);
+        curl_setopt_array($curl, $curlOpt);
+        $response = curl_exec($curl);
+        curl_close($curl);
+        
+        $arrResponse = json_decode($response, TRUE);
+        $arrValues[$SecretName] = $arrResponse["response"];
+      }
+      return $arrValues;
+  }
 
-  $arrResponse = json_decode($response, TRUE);
-  $secValue = $arrResponse["response"];
-  print "<p>The value of that secret is:$secValue[0]</p>";
+  $arrname = array();
+
+  $arrname[] = "MySecret1";
+  $arrname[] = "MySecret2";
+  $arrname[] = "MyFirstSecret";
+  $arrname[] = "/TSC/AnotherTest2";
+  $arrname[] = "/Test/MyPathTest";
+
+  $arrSecretValues = FetchKeylessStatic($arrname);
+
+  print "<p>Here are the secret names and corrensponding values</p>\n";
+  foreach ($arrSecretValues as $key => $value) 
+  {
+    print "$key : $value <br>\n"
+  }
 ?>
