892a35bda8e0a5b73e18217328afdbb17b7b72e2
Siggi@Reba <siggib007@users.noreply.github.com>
2022-08-24 20:59:41 +0000

    License Header

--- index.php
+++ index.php
@@ -1,18 +1,9 @@
 <?php
-  //Copyright © 2009,2015,2022  Siggi Bjarnason.
-  //
-  //This program is free software: you can redistribute it and/or modify
-  //it under the terms of the GNU General Public License as published by
-  //the Free Software Foundation, either version 3 of the License, or
-  //(at your option) any later version.
-  //
-  //This program is distributed in the hope that it will be useful,
-  //but WITHOUT ANY WARRANTY; without even the implied warranty of
-  //MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  //GNU General Public License for more details.
-  //
-  //You should have received a copy of the GNU General Public License
-  //along with this program.  If not, see <http://www.gnu.org/licenses/>
+  /*
+  Copyright © 2009,2015,2022  Siggi Bjarnason.
+  Licensed under GNU GPL v3 and later. Check out LICENSE.TXT for details   
+  or see <https://www.gnu.org/licenses/gpl-3.0-standalone.html>
+  */
 
   require("header.php");
   $strQuery = "SELECT * FROM tblContent WHERE iMenuID = '$iMenuID' and dtTimeStamp = (select max(dtTimeStamp) from tblContent where iMenuID = '$iMenuID');";
